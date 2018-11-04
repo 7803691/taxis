ba62e2f66cbfec50277b798e460ff623a3c8c799
78036391 <7803691@gmail.com>
2018-11-04 16:06:27 +0300

    Second commit

--- src/test/java/com/kdv/taxis/dbApiTest/DbApiBrandServiceTest.java
+++ src/test/java/com/kdv/taxis/dbApiTest/DbApiBrandServiceTest.java
@@ -4,38 +4,37 @@ package com.kdv.taxis.dbApiTest;
         import com.kdv.taxis.dbapi.BrandServiceImpl;
         import com.kdv.taxis.service.IBrandService;
 
-        import org.junit.After;
-        import org.junit.Before;
-        import org.junit.Test;
+
+        import org.apache.log4j.LogManager;
+        import org.apache.log4j.Logger;
+        import org.junit.*;
+
 
         import java.io.IOException;
         import java.util.List;
-        import static org.hamcrest.CoreMatchers.*;
+
+
         import static org.hamcrest.Matchers.hasProperty;
         import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
         import static org.junit.Assert.assertThat;
 
 public class DbApiBrandServiceTest {
+    private static final Logger log = LogManager.getLogger(DbApiBrandServiceTest.class);
 
     private IBrandService service;
+    private Brand testBrand1;
+    private Brand testBrand2;
+    private Brand testBrand3;
+
+
+
 
     @Before
     public void createObject() throws IOException {
         this.service = new BrandServiceImpl();
-
-    }
-
-    @After
-    public void destroyObject(){
-        service = null;
-    }
-
-    @Test
-    public void selectAllBrandTest() throws IOException {
-       List<Brand> allBrands = service.getAllBrands();
-        Brand testBrand1 = new Brand();
-        Brand testBrand2 = new Brand();
-        Brand testBrand3 = new Brand();
+        testBrand1 = new Brand();
+        testBrand2 = new Brand();
+        testBrand3 = new Brand();
         testBrand1.setId(1);
         testBrand1.setName("AUDI");
         testBrand2.setId(2);
@@ -43,13 +42,31 @@ public class DbApiBrandServiceTest {
         testBrand3.setId(3);
         testBrand3.setName("FORD");
 
+    }
+
+    @After
+    public void destroyObject(){
+        service = null;
+        Brand testBrand1 = null;
+        Brand testBrand2 = null;
+        Brand testBrand3 = null;
+
+    }
+
+
+
+
+    @Test
+    public void selectAllBrandTest() throws IOException {
+        List<Brand> allBrands = service.getAllBrands();
         assertThat(allBrands, containsInAnyOrder(
                testBrand1,
                testBrand2,
                testBrand3
-
         ));
+        log.info("selectAllBrandTest complete");
     }
+
 
     @Test
     public void getBrandByIdTest() throws IOException {
